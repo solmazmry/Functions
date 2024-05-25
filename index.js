3c63b24e94573a5c1b0dde87ed537871a6222a31
Solmazmry <167036192+solmazmry@users.noreply.github.com>
2024-05-25 17:10:39 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// function person1(name, surname, age, height , salary, position) {
+//   console.log(`name: ${name}, surname: ${surname} age: ${age}, height: ${height}, salary: ${salary}, position:${position}`);
+
+  
+  
+
+
+// }
+
+// person1("Solmaz", "Mirzayeva",22, 1.64, 2500, "Developer")
+// person1("Nuray", "Mirzayeva",22, 1.64, 2500, "Developer")
+// person1("Solmaz", "Mirzayeva",22, 1.64, 2500, "Developer")
+
+
+
+// 1. Funksiyaya rəqəm ötürün rəqəm cütdürsə ? console.log(number) : alert(number)
+
+// const num = (number)=>{
+// number %2 ==0 ? console.log(number) : alert(number)
+// }
+// num(25)
+
+// 2 dənə funksiya yazın,1ci funksiya daxil edilmiş ədədi 10a vursun,burdan alınan cavabı isə ikinci funksiyaya ötürün həmin nəticənin tək ədəd olub olmamasını yoxlasın.Tək ədədirsə alert-də versin
+
+// const vurma = (num)=>{
+//     yoxlama(num*10)
+// }
+// const yoxlama =(a)=>{
+// a %2==1 ? alert(a) : console.log(a);
+// }
+
+// vurma(22)
+
+
+// Fibonacci alqoritmini yazın,lakin tərsin. Başlanğıc 2 ədəd 1597 və 987 olsun
+
+// const hesablama =(x, y ,operation) =>{
+//     switch (operation) {
+//       case"cixma":
+//         return x-y
+//         break;
+    
+     
+//     }
+// }
+// console.log(hesablama(1597, 987 ,"cixma"));
+
+
+
